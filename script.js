1591359298a979de98dbf2945908fef89f902eaf
Samuel <samuelpedro0001@gmail.com>
2023-05-11 19:50:38 -0300

    projeto frond end concluido

--- script.js
+++ script.js
@@ -13,19 +13,23 @@ const tasks = [
   },
 ];
 
-function createCard(taskInfo) {
+// está função esta renderizando meus cards/tarefas na tela e estou percorrendo por ela para renderizar as tarefas na tela 
+function renderElements(taskList) {
+  const htmlList = document.querySelector(".tasks");
+  htmlList.innerHTML = "";
+
+  for (let i = 0; i < taskList.length; i++) {
+    let card = createCard(taskList[i], tasks, renderElements);
+    htmlList.appendChild(card);
+  }
+}
+
+function createCard(taskInfo, tasks, renderElements) {
   // Criando elementos necessários
-  const li = document.createElement("li");
-  const div = document.createElement("div");
-  const span = document.createElement("span");
-  const p = document.createElement("p");
-
-  // Adicionando o titulo da tarefa como texto do paragrafo
-  p.innerText = taskInfo.titulo;
-
-  // Adicionando span e paragrafo a div
-  div.appendChild(span);
-  div.appendChild(p);
+  const card = document.createElement("li");
+  const container = document.createElement("div");
+  const line = document.createElement("span");
+  const paragraph = document.createElement("p");
 
   // Criando botão para deletar tarefa
   const button = document.createElement("button");
@@ -33,26 +37,57 @@ function createCard(taskInfo) {
   // Adicionando icone ao botão
   button.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
 
-  /// Adicionando a div e o botão de deletar ao list item
-  li.appendChild(div);
-  li.appendChild(button);
+  // Adicionando o titulo da tarefa como texto do paragrafo
+  paragraph.innerText = taskInfo.titulo;
 
-  return li;
-}
+  // Adicionando um evento de clique ao botão
+  button.addEventListener("click", function() {
+    // Procura o objeto referente ao item no array de tasks
+    let index = tasks.indexOf(taskInfo);
 
-function renderElements(taskList) {
-  const htmlList = document.querySelector(".tasks");
-  htmlList.innerHTML = "";
+    if (index !== -1) { // Se o objeto foi encontrado no array
+      // Remove o objeto do array de tasks
+      tasks.splice(index, 1);
+    // Atualiza a lista de tarefas exibidas na tela
+    renderElements(tasks);}
+  });
 
-  // Ajustar a lógica
-  let card = createCard(taskList[0]);
-  htmlList.appendChild(card);
+ // div.appendChild(li) e // Adicionando span e paragrafo a div
+ card.appendChild(line);
+ container.appendChild(paragraph);
+ container.appendChild(line);
+ container.appendChild(paragraph);
 
-  card = createCard(taskList[1]);
-  htmlList.appendChild(card);
+  // Adicionando a div e o botão de deletar ao list item
+  card.appendChild(container);
+  card.appendChild(button);
 
-  card = createCard(taskList[2]);
-  htmlList.appendChild(card);
+  // verificando e adicionando as classes as tarefas ja existentes e as que foram adicionadas
+
+  if (taskInfo.tipo === "Urgente") {
+    line.classList.add("span-urgent");
+  } else if (taskInfo.tipo === "Prioritário") {
+    line.classList.add("span-priority");
+  } else {
+    line.classList.add("span-normal");
+  }
+
+  return card;
 }
 
 renderElements(tasks);
+
+// selecionando o botão de adicionar tarefas e criando um evento de adicionar tarefas 
+
+let addButton = document.querySelector("#btnSubmit");
+addButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  let input = document.querySelector("#input_title");
+  let inputtype = document.querySelector("#input_priority");
+  let title = input.value;
+  let type = inputtype.value;
+  let task = { titulo: title, tipo: type };
+  tasks.push(task);
+  renderElements(tasks);
+});
+
